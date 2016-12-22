fc87a0e1183eca38c51f5cfcb22de25b59186b96
Saif <saif@mxd3d.com>
2016-12-22 12:04:45 +0200

    changed deps

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Arc.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.8"}, # Required for downloading remote files
-      {:ex_aws, "~> 1.0.0", optional: true},
+      {:ex_aws, git: "git://github.com/MixedDimensionsLib/ex_aws.git"},
       {:mock, "~> 0.1.1", only: :test},
       {:ex_doc, "~> 0.14", only: :dev},
 
