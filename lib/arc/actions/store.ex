b496577eb8d7b7ef5cf74f394133b9f80f4e696a
safwan <safwan@mxd3d.com>
2018-09-25 10:47:59 +0300

    remove hashed file

--- lib/arc/actions/store.ex
+++ lib/arc/actions/store.ex
@@ -17,29 +17,33 @@ defmodule Arc.Actions.Store do
   # Private
   #
 
-  defp put(_definition, { error = {:error, _msg}, _scope}), do: error
-  defp put(definition, {%Arc.File{}=file, scope}) do
+  defp put(_definition, {error = {:error, _msg}, _scope}), do: error
+
+  defp put(definition, {%Arc.File{} = file, scope}) do
     case definition.validate({file, scope}) do
       true -> put_versions(definition, {file, scope})
-      _    -> {:error, :invalid_file}
+      _ -> {:error, :invalid_file}
     end
   end
 
   defp put_versions(definition, {file, scope}) do
     if definition.async do
       definition.__versions
-      |> Enum.map(fn(r)    -> async_put_version(definition, r, {file, scope}) end)
-      |> Enum.map(fn(task) -> Task.await(task, version_timeout()) end)
+      |> Enum.map(fn r -> async_put_version(definition, r, {file, scope}) end)
+      |> Enum.map(fn task -> Task.await(task, version_timeout()) end)
       |> handle_responses(file.file_name)
     else
       definition.__versions
-      |> Enum.map(fn(version) -> put_version(definition, version, {file, scope}) end)
+      |> Enum.map(fn version -> put_version(definition, version, {file, scope}) end)
       |> handle_responses(file.file_name)
     end
   end
 
   defp handle_responses(responses, filename) do
-    errors = Enum.filter(responses, fn(resp) -> elem(resp, 0) == :error end) |> Enum.map(fn(err) -> elem(err, 1) end)
+    errors =
+      Enum.filter(responses, fn resp -> elem(resp, 0) == :error end)
+      |> Enum.map(fn err -> elem(err, 1) end)
+
     if Enum.empty?(errors), do: {:ok, filename}, else: {:error, errors}
   end
 
@@ -55,11 +59,30 @@ defmodule Arc.Actions.Store do
 
   defp put_version(definition, version, {file, scope}) do
     case Arc.Processor.process(definition, version, {file, scope}) do
-      {:error, error} -> {:error, error}
+      {:error, error} ->
+        {:error, error}
+
       {:ok, file} ->
-        file_name = Arc.Definition.Versioning.resolve_file_name(definition, version, {file, scope})
-        file      = %Arc.File{file | file_name: file_name}
-        definition.__storage.put(definition, version, {file, scope})
+        # file |> IO.inspect(label: "before resolve")
+
+        file_name =
+          Arc.Definition.Versioning.resolve_file_name(definition, version, {file, scope})
+
+        file = %Arc.File{file | file_name: file_name}
+        # file |> IO.inspect(label: "after resolve")
+
+        res = definition.__storage.put(definition, version, {file, scope})
+
+        case Path.basename(file.path) == file_name || Path.dirname(file.path) != "/tmp" do
+          true ->
+            file |> IO.inspect(label: "YEEEEEEEES")
+
+          false ->
+            File.rm(file.path)
+            file |> IO.inspect(label: "NOOOOOOOOO")
+        end
+
+        res
     end
   end
 end
--- lib/arc/processor.ex
+++ lib/arc/processor.ex
@@ -5,12 +5,15 @@ defmodule Arc.Processor do
   end
 
   defp apply_transformation(file, :noaction), do: {:ok, file}
-  defp apply_transformation(file, {:noaction}), do: {:ok, file} # Deprecated
+  # Deprecated
+  defp apply_transformation(file, {:noaction}), do: {:ok, file}
+
   defp apply_transformation(file, {cmd, conversion, _}) do
     apply_transformation(file, {cmd, conversion})
   end
 
   defp apply_transformation(file, {cmd, conversion}) do
     Arc.Transformations.Convert.apply(cmd, Arc.File.ensure_path(file), conversion)
+    # |> IO.inspect()
   end
 end
